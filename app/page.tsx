72fa1a2416b2b0dc0286ab4824c9be4972b6c480
gayashan4lk <gayashan4lk@gmail.com>
2024-02-11 04:16:12 +0530

    optimize images

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
@@ -29,7 +30,8 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image src='/hero-desktop.png' width={1000} height={760} className='hidden md:block' alt='Desktop version dashboard' />
+          <Image src='/hero-mobile.png' width={560} height={620} className='block md:hidden' alt='Mobile version dashboard' />
         </div>
       </div>
     </main>
