5ffb2df741eb93abbf13f9766280e54016a3fdb1
gayashan4lk <gayashan4lk@gmail.com>
2024-02-11 07:12:14 +0530

    highlight active links

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 import {
   UserGroupIcon,
   HomeIcon,
@@ -14,6 +18,8 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathName = usePathname();
+
   return (
     <>
       {links.map((link) => {
@@ -22,7 +28,7 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx('flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3', { 'bg-sky-100 text-blue-600': pathName === link.href })}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
