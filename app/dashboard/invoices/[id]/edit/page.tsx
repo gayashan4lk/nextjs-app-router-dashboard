8a95e0d7b1392f2ea546f9aae6cc3082fc0790aa
gayashan4lk <gayashan4lk@gmail.com>
2024-02-16 20:14:10 +0530

    add not found fallback

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import Form from '@/app/ui/invoices/edit-form';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
 	const id = params.id;
@@ -9,6 +10,10 @@ export default async function Page({ params }: { params: { id: string } }) {
 		fetchInvoiceById(id),
 		fetchCustomers()
 	])
+
+	if (!invoice) {
+		notFound();
+	}
 
 	return <main>
 		<Breadcrumbs breadcrumbs={[
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -63,7 +63,8 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Mock error: Failed to delete invoice.');
+  // Throwing mock error for testing purposes
+  // throw new Error('Mock error: Failed to delete invoice.');
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (error) {
